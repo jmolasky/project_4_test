b12a247755312222b27af817cc4951c2a103e992
jmolasky <jeromemolasky@gmail.com>
2022-03-16 02:38:46 -0500

    adds single-page index view logic

--- main_app/views.py
+++ main_app/views.py
@@ -19,8 +19,65 @@ def home(request):
     return render(request, 'home.html')
 
 def wallets_index(request):
+    # get all wallets
     wallets = Wallet.objects.all()
-    return render(request, 'wallets/index.html', {'wallets': wallets})
+    symbols_arr = []
+    wallets_arr = []
+    for wallet in wallets:
+        coins_arr = []
+        wallet_coins = Amount.objects.filter(wallet=wallet.id)
+        for coin in wallet_coins:
+            if (coin.crypto.symbol not in symbols_arr):
+                symbols_arr.append(coin.crypto.symbol)
+            coin_object = {
+                'symbol': coin.crypto.symbol,
+                'amount': coin.amount,
+            }
+            coins_arr.append(coin_object)
+        wallet_obj = {
+            'name': wallet.name,
+            'coins': coins_arr,
+        }
+        # add coins_not_in_wallet to each wallet
+        wallet_obj['coins_not_in_wallet'] =  CryptoCurrency.objects.exclude(id__in=wallet_coins.values_list('crypto'))
+        wallets_arr.append(wallet_obj)
+    # get all symbols in all wallets
+    symbols = ','.join(symbols_arr)
+    parameters = {
+        'symbol': symbols
+    }
+    session = Session()
+    session.headers.update(headers)
+    try:
+        # api call for all coins in all wallets
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        data = data['data']
+        coins = []
+        for symbol in symbols_arr:
+            coin_obj = data[symbol][0]
+            obj = {
+                'symbol': symbol,
+                'name': coin_obj['name'],
+                'last_updated': coin_obj['last_updated'],
+                'quote': coin_obj['quote']['USD'],
+            }
+            coins.append(obj)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+    
+    # for wallet in wallets_arr:
+    #     for coin in wallet['coins']:
+    #       coin['price'] = 
+    for wallet in wallets_arr:
+        for coin in wallet['coins']:
+            for obj in coins:
+                if obj['symbol'] == coin['symbol']:
+                    coin['price'] = obj['quote']['price']
+    print(wallets_arr)
+    return render(request, 'wallets/index.html', {
+        'wallets': wallets,
+    })
 
 def wallets_detail(request, wallet_id):
     wallet = Wallet.objects.get(id=wallet_id)
